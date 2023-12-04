895ee725ed3bcab38b5e0384fbd00ef0b4f6a679
CaptainVoronin <CaptainVoronin@gmail.com>
2023-12-04 15:35:42 +0300

    Добавлено создание ViewMaker'a для виджетов с ненулевым ограничением бюджета

--- app/src/main/java/org/max/budgetcontrol/ViewMakerFactory.java
+++ app/src/main/java/org/max/budgetcontrol/ViewMakerFactory.java
@@ -17,10 +17,17 @@ public class ViewMakerFactory
    {
       switch( httpCode ) {
          case 200:
-            return new BaseWidgetViewMaker1(context, widget);
+            return getViewFor200Code( widget );
          case 401:
          default:
             return new NoConnectionViewMaker( context, widget );
       }
+   }
+
+   private AWidgetViewMaker getViewFor200Code(WidgetParams widget) {
+      if( widget.getLimitAmount() > 1 )
+         return new AmountLimitViewMaker( context, widget );
+      else
+         return new BaseWidgetViewMaker1(context, widget);
    }
 }
