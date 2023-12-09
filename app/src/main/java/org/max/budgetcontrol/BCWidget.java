325fa06e8430d5b205261f28e0322569cfa1ec02
CaptainVoronin <CaptainVoronin@gmail.com>
2023-12-10 02:24:15 +0300

    Добавлена onDeleted на удаление экземпляра виджета.

--- app/src/main/java/org/max/budgetcontrol/BCWidget.java
+++ app/src/main/java/org/max/budgetcontrol/BCWidget.java
@@ -33,43 +33,48 @@ public class BCWidget extends AppWidgetProvider
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
     {
-        String strIds = Arrays.stream( appWidgetIds ).mapToObj( id -> Integer.toString( id )).collect(Collectors.joining(","));
-        Log.i( this.getClass().getName(), "[onUpdate] Widgets update. ID list " + strIds );
-        try {
+        String strIds = Arrays.stream(appWidgetIds).mapToObj(id -> Integer.toString(id)).collect(Collectors.joining(","));
+        Log.i(this.getClass().getName(), "[onUpdate] Widgets update. ID list " + strIds);
+        try
+        {
             initApp(context);
-        } catch (MalformedURLException e) {
+        } catch (MalformedURLException e)
+        {
             throw new RuntimeException(e);
-        } catch (JSONException e) {
+        } catch (JSONException e)
+        {
             throw new RuntimeException(e);
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             throw new RuntimeException(e);
         }
 
-        BCDBHelper db = BCDBHelper.getInstance( context );
-        List<WidgetParams> widgets = db.getWidgets( appWidgetIds );
+        BCDBHelper db = BCDBHelper.getInstance(context);
+        List<WidgetParams> widgets = db.getWidgets(appWidgetIds);
 
         // При создании виджета его ID уже существует,
         // а сам виджет еще нет. Его нет и в БД.
         // Соответственно, и обновлять пока нечего.
         // Поэтому просто выходим
-        if( widgets.size() == 0 )
+        if (widgets.size() == 0)
         {
-            Log.i( this.getClass().getName(), "[onUpdate] Nothing to update. Exit function");
+            Log.i(this.getClass().getName(), "[onUpdate] Nothing to update. Exit function");
             return;
         }
 
         StartPeriodEncoding period = StartPeriodEncoding.year;
 
-        Collections.sort(widgets, (a, b) -> Integer.compare( a.getStartPeriod().number(), b.getStartPeriod().number() ));
-        StartPeriodEncoding maxPeriod = widgets.get( widgets.size() - 1 ).getStartPeriod();
-        long timestamp = AWidgetViewMaker.calculateStartDate( maxPeriod ) / 1000l;
+        Collections.sort(widgets, (a, b) -> Integer.compare(a.getStartPeriod().number(), b.getStartPeriod().number()));
+        StartPeriodEncoding maxPeriod = widgets.get(widgets.size() - 1).getStartPeriod();
+        long timestamp = AWidgetViewMaker.calculateStartDate(maxPeriod) / 1000l;
 
-        UpdateSelectedWidgetsHandler handler = new UpdateSelectedWidgetsHandler( context, appWidgetManager, appWidgetIds, widgets );
-        ZenMoneyClient client = new ZenMoneyClient( url, token, handler );
-        client.loadTransactions( timestamp );
+        UpdateSelectedWidgetsHandler handler = new UpdateSelectedWidgetsHandler(context, appWidgetManager, appWidgetIds, widgets);
+        ZenMoneyClient client = new ZenMoneyClient(url, token, handler);
+        client.loadTransactions(timestamp);
     }
 
-    private void updateWithError(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds, JSONException e) {
+    private void updateWithError(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds, JSONException e)
+    {
 
     }
 
@@ -78,18 +83,27 @@ public class BCWidget extends AppWidgetProvider
     {
     }
 
-    private void initApp(Context context) throws JSONException, IOException {
-        SettingsHolder settingsHolder = new SettingsHolder( context );
+    @Override
+    public void onDeleted(Context context, int[] appIds)
+    {
+        BCDBHelper db = BCDBHelper.getInstance(context);
+        db.deleteWidgetsByAppId(appIds);
+        super.onDeleted(context, appIds);
+    }
+
+    private void initApp(Context context) throws JSONException, IOException
+    {
+        SettingsHolder settingsHolder = new SettingsHolder(context);
         settingsHolder.init();
-        token = settingsHolder.getParameterAsString( "token" );
-        String strURL = settingsHolder.getParameterAsString( "url" );
-        url = new URL( strURL );
+        token = settingsHolder.getParameterAsString("token");
+        String strURL = settingsHolder.getParameterAsString("url");
+        url = new URL(strURL);
     }
 
     @Override
     public void onDisabled(Context context)
     {
-        BCDBHelper bcdbHelper = BCDBHelper.getInstance(  context );
+        BCDBHelper bcdbHelper = BCDBHelper.getInstance(context);
         bcdbHelper.clearWidgets();
     }
 
