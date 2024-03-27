437912eb48ff16ca6ac1dba8ade58df4d04ef0b8
CaptainVoronin <CaptainVoronin@gmail.com>
2024-03-27 23:49:37 +0300

    Dialog works right

--- app/src/main/java/org/max/budgetcontrol/DlgColorSelector.java
+++ app/src/main/java/org/max/budgetcontrol/DlgColorSelector.java
@@ -1,6 +1,5 @@
 package org.max.budgetcontrol;
 
-import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
@@ -8,70 +7,93 @@ import android.view.View;
 import android.widget.ImageView;
 
 import org.max.budgetcontrol.zentypes.WidgetParams;
+import org.max.preditor.editors.ColorDialog;
 
 import androidx.appcompat.app.AlertDialog;
 
 class DlgColorSelector
 {
-    private final WidgetParams.LabelParams currentParams;
     private final String title;
     MainActivity mainActivity;
 
     private OnOkListener listener;
 
+/*
     GetSingleColorListener textColorListener;
     GetSingleColorListener bgColorListener;
 
     ValueHolder<Integer> textColorHolder;
     ValueHolder<Integer> bgColorHolder;
 
-    int textColor;
-    int bgColor;
+*/
+    int newTextColor;
+    int newBgColor;
 
 
     public DlgColorSelector(MainActivity mainActivity, WidgetParams.LabelParams currentParams, OnOkListener listener, String title)
     {
         this.mainActivity = mainActivity;
-        this.currentParams = currentParams;
         this.listener = listener;
-        textColor = currentParams.getFontColor().toArgb();
-        bgColor = currentParams.getBackColor().toArgb();
+        newTextColor = currentParams.getFontColor().toArgb();
+        newBgColor = currentParams.getBackColor().toArgb();
         this.title = title;
     }
 
     public void show()
     {
-        textColorHolder = new ValueHolder<>(textColor);
-        bgColorHolder = new ValueHolder<>(bgColor);
-
-        textColorListener = new GetSingleColorListener(textColorHolder);
-        bgColorListener = new GetSingleColorListener(bgColorHolder);
-
         AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
         builder.setNegativeButton(android.R.string.cancel, (dialog, i) -> dialog.cancel());
-        builder.setTitle( title );
+        builder.setTitle(title);
 
         LayoutInflater inflater = mainActivity.getLayoutInflater();
         View v = inflater.inflate(R.layout.dlg_color_set, null);
 
-        ImageView imv = v.findViewById(R.id.imageViewColorText);
-        imv.setOnClickListener(textColorListener);
+        ImageView imvText = v.findViewById(R.id.imageViewColorText);
+        imvText.setOnClickListener(v1 -> {
+            ColorDialog dialog = new ColorDialog(mainActivity);
+            dialog.setSelectedColor(newTextColor);
+            dialog.setPositiveButtonListener(new ColorDialog.OnClickListener()
+            {
+                @Override
+                public void onButtonClick(int selectedColor)
+                {
+                    newTextColor = selectedColor;
+                    GradientDrawable drw = (GradientDrawable) imvText.getBackground();
+                    drw.setColor(newTextColor);
+                }
+            });
+            dialog.show();
+        });
 
-        GradientDrawable drw = (GradientDrawable) imv.getBackground();
-        drw.setColor(textColor);
+        GradientDrawable drw = (GradientDrawable) imvText.getBackground();
+        drw.setColor(newTextColor);
 
-        imv = v.findViewById(R.id.imageViewColorBackground);
-        imv.setOnClickListener(bgColorListener);
+        ImageView imvBack = v.findViewById(R.id.imageViewColorBackground);
+        imvBack.setOnClickListener(v1 -> {
+            ColorDialog dialog = new ColorDialog(mainActivity);
+            dialog.setSelectedColor(newBgColor);
+            dialog.setPositiveButtonListener(new ColorDialog.OnClickListener()
+            {
+                @Override
+                public void onButtonClick(int selectedColor)
+                {
+                    newBgColor = selectedColor;
+                    GradientDrawable drw = (GradientDrawable) imvBack.getBackground();
+                    drw.setColor(newBgColor);
+                }
+            });
+            dialog.show();
+        });
 
-        drw = (GradientDrawable) imv.getBackground();
-        drw.setColor(bgColor);
+        drw = (GradientDrawable) imvBack.getBackground();
+        drw.setColor(newBgColor);
 
         builder.setView(v);
 
         builder.setPositiveButton(android.R.string.ok, (dialog, i) -> {
-            WidgetParams.LabelParams newParams =
-                    new WidgetParams.LabelParams(Color.valueOf(bgColorHolder.getValue()), Color.valueOf(textColorHolder.getValue()));
-            listener.okPressed(newParams);
+            WidgetParams.LabelParams params = new WidgetParams.LabelParams( Color.valueOf(newBgColor),
+                    Color.valueOf(newTextColor) );
+            listener.okPressed(params);
         });
         builder.show();
     }
@@ -79,41 +101,5 @@ class DlgColorSelector
     public interface OnOkListener
     {
         void okPressed(WidgetParams.LabelParams newLabelParams);
-    }
-
-    class GetSingleColorListener implements View.OnClickListener
-    {
-        ValueHolder<Integer> valueHolder;
-
-        GetSingleColorListener(ValueHolder<Integer> vHolder)
-        {
-            this.valueHolder = vHolder;
-        }
-
-        @Override
-        public void onClick(View v)
-        {
-            valueHolder.setValue( Color.RED );
-        }
-    }
-
-    class ValueHolder<T>
-    {
-        T value;
-
-        public ValueHolder(T value)
-        {
-            this.value = value;
-        }
-
-        public void setValue(T value)
-        {
-            this.value = value;
-        }
-
-        public T getValue()
-        {
-            return value;
-        }
     }
 }
